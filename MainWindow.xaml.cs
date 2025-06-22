84312277285fb516ff338672c1af7c619053ebb4
M G M <madegraham@gmail.com>
2025-06-22 12:47:16 -0400

    Make workable version of player switching mechanic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,15 +26,23 @@ namespace TimeKeepersConcept
         int hundredthsOfSecondsToGo;
         int score = 0;
         int totalScore = 0;
+        Era era;
+        PlayerPlaying playerPlaying;
+        int roundTracker;
         public MainWindow()
         { 
             InitializeComponent();
+
             totalScoreBox.Text = "00";
             totalScoreBox1.Text = "00";
             scoreBox.Text = "00";
             scoreBox1.Text = "00";
             timeTextBlock.Text = "10:00";
             timeTextBlock1.Text = "10:00";
+
+            era = Era.era1;
+            playerPlaying = PlayerPlaying.player1Playing;
+            roundTracker = 1;
 
             timer.Interval = TimeSpan.FromSeconds(0.01);
             timer.Tick += Timer_Tick;
@@ -56,34 +64,29 @@ namespace TimeKeepersConcept
             final
         };
 
-        //private void runEra()
-        //{
-        //    switch {
-        //        case era.era1
-        //        {
-
-        //        };
-        //            case era.era2
-        //            {
-
-        //            };
-        //            case era.era3
-        //            {
-
-        //            }
-        //    }
-        //}
-        
         private void Timer_Tick(object sender, EventArgs e)
         {
             hundredthsOfSecondsToGo--;
-            timeTextBlock.Text = (hundredthsOfSecondsToGo / 100F).ToString("00.00");
-            if (hundredthsOfSecondsToGo == 0)
+            if (playerPlaying == PlayerPlaying.player1Playing)
             {
-                timer.Stop();
-                timeTextBlock.Text = "Time's up!";
-            }
+                timeTextBlock.Text = (hundredthsOfSecondsToGo / 100F).ToString("00.00");
 
+                if (hundredthsOfSecondsToGo == 0)
+                {
+                    timer.Stop();
+                    timeTextBlock.Text = "Time's up!";
+                }
+            }
+            else
+            {
+                timeTextBlock1.Text = (hundredthsOfSecondsToGo / 100F).ToString("00.00");
+
+                if (hundredthsOfSecondsToGo == 0)
+                {
+                    timer.Stop();
+                    timeTextBlock1.Text = "Time's up!";
+                }
+            }
         }
 
         private void Stop_Reset_Timer()
@@ -92,6 +95,15 @@ namespace TimeKeepersConcept
             timer.Start();
             hundredthsOfSecondsToGo = 10 * 100;
 
+        }
+
+        public void Switch_Player()
+        {
+            if (playerPlaying == PlayerPlaying.player1Playing)
+            {
+                playerPlaying = PlayerPlaying.player2Playing;
+            }
+            else { playerPlaying = PlayerPlaying.player1Playing; }
         }
 
         private void stopGameButton_Click(object sender, RoutedEventArgs e)
@@ -104,19 +116,52 @@ namespace TimeKeepersConcept
 
         private void pushButton_Click(object sender, RoutedEventArgs e)
         {
-            addScore();
-            Stop_Reset_Timer();
+            // When you click on player1's button, it either does nothing,
+            // or adds score if it's the player 1's turn
+            if (playerPlaying == PlayerPlaying.player2Playing) { return; }
+            else
+            {
+                addScore();
+                Stop_Reset_Timer();
+                Switch_Player();
+            }
             
         }
+        private void pushButton1_Click(object sender, RoutedEventArgs e)
+        {
+            // When you click on player1's button, it either does nothing,
+            // or adds score if it's the player 1's turn
+            if (playerPlaying == PlayerPlaying.player1Playing) { return; }
+            else
+            {
+                addScore();
+                Stop_Reset_Timer();
+                Switch_Player();
+            }
+
+        }
+
 
         public void addScore()
         {
-            /// Calculates score to be distance from the next whole integer
+            // Calculates score to be distance from the next whole integer
             score = (hundredthsOfSecondsToGo) % 100;
             if (score > 50) { score = 100 - score; }
-            scoreBox.Text = score.ToString();
-            totalScore += score;
-            totalScoreBox.Text = totalScore.ToString();
+            // adds the score to whomever's turn it is
+            if (playerPlaying == PlayerPlaying.player1Playing)
+            {
+                scoreBox.Text = score.ToString();
+                totalScore += score;
+                totalScoreBox.Text = totalScore.ToString();
+            }
+            else
+            {
+                scoreBox1.Text = score.ToString();
+                totalScore += score;
+                totalScoreBox1.Text = totalScore.ToString();
+            }
+            // increments the roundTracker
+            roundTracker++;
         }
 
     }
