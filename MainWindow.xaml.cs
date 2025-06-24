bc1d285a3a2f1436d2d6ef7519d51339cc596c1e
M G M <madegraham@gmail.com>
2025-06-24 13:38:07 -0400

    fixes Dispatcher timer issue by using datetime, adds era handling that doesn't work yet

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Diagnostics;
 
 namespace TimeKeepersConcept
 {
@@ -26,9 +27,12 @@ namespace TimeKeepersConcept
         int hundredthsOfSecondsToGo;
         int score = 0;
         int totalScore = 0;
+        int totalScore1 = 0;
         Era era;
         PlayerPlaying playerPlaying;
+        bool timerRunning = false;
         int roundTracker;
+        DateTime datetime;
         public MainWindow()
         { 
             InitializeComponent();
@@ -40,14 +44,14 @@ namespace TimeKeepersConcept
             timeTextBlock.Text = "10:00";
             timeTextBlock1.Text = "10:00";
 
+            // instantiate playing state
             era = Era.era1;
             playerPlaying = PlayerPlaying.player1Playing;
             roundTracker = 1;
 
-            timer.Interval = TimeSpan.FromSeconds(0.01);
-            timer.Tick += Timer_Tick;
-            timer.Start();
-            hundredthsOfSecondsToGo = 10 * 100;
+            // instatiate timer
+            timer.Tick += new EventHandler(Timer_Tick);
+            timer.Interval = TimeSpan.FromMilliseconds(10);
         }
 
         enum PlayerPlaying
@@ -65,26 +69,75 @@ namespace TimeKeepersConcept
         };
 
         private void Timer_Tick(object sender, EventArgs e)
+        { 
+            if (timerRunning)
+            {
+                while (roundTracker <= 3)
+                {
+                    era = Era.era1;
+                    RunRounds();
+                }
+                while ((roundTracker >4) && (roundTracker <= 6))
+                {
+                    era = Era.era2;
+                    RunRounds();
+                }
+                while ((roundTracker > 6) && (roundTracker <= 9))
+                {
+                    era = Era.era3;
+                    RunRounds();
+                }
+                if (roundTracker == 10)
+                {
+                    era = Era.final;
+                    RunRounds();
+                }
+            }
+        }
+
+        private void RunRounds()
         {
-            hundredthsOfSecondsToGo--;
-            if (playerPlaying == PlayerPlaying.player1Playing)
+            int elapsedHundredthsOfSeconds = (int)(datetime - DateTime.Now).TotalMilliseconds / 10;
+            hundredthsOfSecondsToGo = 10 * 100 + elapsedHundredthsOfSeconds;
+            // the counters go down together when it's the final round
+            if (era == Era.final)
             {
                 timeTextBlock.Text = (hundredthsOfSecondsToGo / 100F).ToString("00.00");
-
-                if (hundredthsOfSecondsToGo == 0)
+                timeTextBlock1.Text = (hundredthsOfSecondsToGo / 100F).ToString("00.00");
+                if (hundredthsOfSecondsToGo <= 0)
                 {
-                    timer.Stop();
-                    timeTextBlock.Text = "Time's up!";
+
+                    timeTextBlock.Text = "Time's up!";       
+                    timeTextBlock1.Text = "Time's up!";
+                    addScore();
                 }
             }
             else
             {
-                timeTextBlock1.Text = (hundredthsOfSecondsToGo / 100F).ToString("00.00");
-
-                if (hundredthsOfSecondsToGo == 0)
+                if (playerPlaying == PlayerPlaying.player1Playing)
                 {
-                    timer.Stop();
-                    timeTextBlock1.Text = "Time's up!";
+                    timeTextBlock.Text = (hundredthsOfSecondsToGo / 100F).ToString("00.00");
+
+                    if (hundredthsOfSecondsToGo <= 0)
+                    {
+
+                        timeTextBlock.Text = "Time's up!";
+                        addScore();
+                        Stop_Reset_Timer();
+                        Switch_Player();
+                    }
+                }
+                else
+                {
+                    timeTextBlock1.Text = (hundredthsOfSecondsToGo / 100F).ToString("00.00");
+
+                    if (hundredthsOfSecondsToGo <= 0)
+                    {
+                        timeTextBlock1.Text = "Time's up!";
+                        addScore();
+                        Stop_Reset_Timer();
+                        Switch_Player();
+                    }
                 }
             }
         }
@@ -92,9 +145,8 @@ namespace TimeKeepersConcept
         private void Stop_Reset_Timer()
         {
             timer.Stop();
+            datetime = DateTime.Now;
             timer.Start();
-            hundredthsOfSecondsToGo = 10 * 100;
-
         }
 
         public void Switch_Player()
@@ -108,9 +160,20 @@ namespace TimeKeepersConcept
 
         private void stopGameButton_Click(object sender, RoutedEventArgs e)
         {
-            // 
-            timer.Stop();
-            timeTextBlock.Text = "Game Stopped";
+            // starts the game if it's not running
+            if (!timerRunning)
+            { 
+                datetime = DateTime.Now;
+                timer.Start();
+                timerRunning = true;
+            }
+            else
+            {
+                timer.Stop();
+                timerRunning = false;
+                timeTextBlock.Text = "Game Paused";
+                timeTextBlock1.Text = "Game Paused";
+            }
             
         }
 
@@ -142,27 +205,43 @@ namespace TimeKeepersConcept
         }
 
 
-        public void addScore()
+        private void addScore()
         {
             // Calculates score to be distance from the next whole integer
             score = (hundredthsOfSecondsToGo) % 100;
             if (score > 50) { score = 100 - score; }
             // adds the score to whomever's turn it is
-            if (playerPlaying == PlayerPlaying.player1Playing)
+            if (hundredthsOfSecondsToGo <= 0) { score = 50; }
+            if (era == Era.final)
             {
                 scoreBox.Text = score.ToString();
-                totalScore += score;
-                totalScoreBox.Text = totalScore.ToString();
-            }
-            else
-            {
                 scoreBox1.Text = score.ToString();
                 totalScore += score;
-                totalScoreBox1.Text = totalScore.ToString();
+                totalScore1 += score;
+                totalScoreBox.Text = totalScore.ToString();
+                totalScoreBox1.Text = totalScore1.ToString();
             }
-            // increments the roundTracker
-            roundTracker++;
+            else {
+                if (playerPlaying == PlayerPlaying.player1Playing)
+                {
+                    scoreBox.Text = score.ToString();
+                    totalScore += score;
+                    totalScoreBox.Text = totalScore.ToString();
+                }
+                else
+                {
+                    scoreBox1.Text = score.ToString();
+                    totalScore1 += score;
+                    totalScoreBox1.Text = totalScore1.ToString();
+                }
+                // increments the roundTracker
+                roundTracker++;
+            }
         }
 
+        private void eraChecker()
+        {
+            // put brushes in here to indicate what era/round it is
+        }
     }
 }
